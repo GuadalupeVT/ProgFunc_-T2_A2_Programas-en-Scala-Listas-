e5eb09fd6a5270a9d4842a4a689a159943586f9e
Guadalupe Vazquez <guvadlt@gmail.com>
2021-03-04 18:32:51 -0600

    Se agrego la funcion de suma acumulada

--- src/Prueba.scala
+++ src/Prueba.scala
@@ -7,7 +7,7 @@ object Prueba {
   def crearLista(p:Int): ListBuffer[String] ={
     var listaPalabras = new ListBuffer[String]()
     println("¿Cuantas palabras deseas ingresar a la lista "+p+"?")
-    val n=readInt()
+    var n=readInt()
     for(i <- 0 until n){
       println("Ingresa palabra "+(i+1)+": ")
       listaPalabras+=readLine()
@@ -19,7 +19,7 @@ object Prueba {
   def crearListaNum(): ListBuffer[Int]={
     var lista = new ListBuffer[Int]()
     println("¿Cuantas numeros deseas en la lista?")
-    val n=readInt()
+    var n=readInt()
     for(i <- 0 until n){
       lista+=(math.random * (100-10) + 20).toInt
     }
@@ -49,8 +49,6 @@ object Prueba {
   //palabras y sustituya la primera por la segunda en la lista.
   def sustitucionEnLaLista(palabra1:String, palabra2:String,lista: => ListBuffer[String]): ListBuffer[String] ={
     var sum=0
-    print("La lista original es: ")
-    print(lista)
     for(i <- 0 until lista.size){
       if(lista(i)== palabra1){
         sum+=1
@@ -58,11 +56,6 @@ object Prueba {
         lista.insert(i,palabra2)
       }
     }
-    println()
-    println("La palabra "+palabra1+" aparecio en la lista "+sum+" veces")
-    print("La lista modificada es: ")
-    print(lista)
-    println()
     lista
   }
 
@@ -71,8 +64,6 @@ object Prueba {
   //palabra y elimine esa palabra de la lista.
   def eliminarPalabra(palabra:String,lista: => ListBuffer[String]): ListBuffer[String] ={
     var sum=0
-    print("La lista original es: ")
-    print(lista)
     for(i <- 0 until lista.size){
       if(lista(i)== palabra){
         sum+=1
@@ -81,11 +72,6 @@ object Prueba {
     for(e <- 0 until sum){
       lista-=palabra
     }
-    println()
-    println("La palabra "+palabra+" aparecio en la lista "+sum+" veces")
-    print("La lista modificada es: ")
-    print(lista)
-    println()
     lista
   }
 
@@ -95,7 +81,6 @@ object Prueba {
     var list=lista1
     for(i <- 0 until lista2.size){
       list=eliminarPalabra(lista2(i),lista1)
-      println("...........................")
     }
     list
   }
@@ -104,9 +89,26 @@ object Prueba {
   //5. Escriba un programa que permita crear dos listas de palabras y que, a continuación, escriba las
   //siguientes listas (en las que no debe haber repeticiones):
   //Lista de palabras que aparecen en las dos listas.
+  def unionDeListas(lista1: => ListBuffer[String], lista2: => ListBuffer[String]): ListBuffer[String] ={
+    var listaPalabrasUnidas = lista1
+    for(e <- 0 until lista2.size) {
+      listaPalabrasUnidas+=lista2(e)
+    }
+    listaPalabrasUnidas
+  }
   //Lista de palabras que aparecen en la primera lista, pero no en la segunda.
   //Lista de palabras que aparecen en la segunda lista, pero no en la primera.
   //Lista de palabras que aparecen en ambas listas.
+  def unionDeListasSinRep(lista1: => ListBuffer[String], lista2: => ListBuffer[String]): ListBuffer[String] ={
+    var listaN1=eliminarPalabrasEnLaPrimeraDeLaSegunda(lista1,lista2)
+    var listaN2=eliminarPalabrasEnLaPrimeraDeLaSegunda(lista2,lista1)
+    var listaPalabrasUnidasSinRep = ListBuffer[String]()
+    listaPalabrasUnidasSinRep=listaN1
+    for(e <- 0 until listaN2.size) {
+      listaPalabrasUnidasSinRep+=listaN2(e)
+    }
+    listaPalabrasUnidasSinRep
+  }
   //Nota: Para evitar las repeticiones, el programa deberá empezar eliminando los elementos
   //repetidos en cada lista.
 
@@ -115,6 +117,17 @@ object Prueba {
   //nueva lista donde el primer elemento es el mismo, el segundo elemento es la suma del primero
   //con el segundo, el tercer elemento es la suma del resultado anterior con el siguiente elemento y así
   //sucesivamente. Por ejemplo, la suma acumulada de [1,2,3] es [1, 3, 6].
+  def listaSumaAculumada(lista: => ListBuffer[Int]):ListBuffer[Int]={
+    var listaSuma = new ListBuffer[Int]()
+    var sum=0
+    for(e <- 0 until lista.size) {
+      sum+=lista(e)
+      listaSuma+=sum
+      //println(sum)
+    }
+    //println(sum)
+    listaSuma
+  }
 
 
   //7. Escribe una función llamada “obtenerPalindromas" que busque todas las palabras palíndromas de
@@ -138,6 +151,8 @@ object Prueba {
     var listaPalabras1=crearLista(1)
     var listaPalabras2=crearLista(2)
     var listaNumeros=crearListaNum()
+    println(listaPalabras1)
+    println(listaPalabras2)
 
     //Impresion de menu
     var menu=0
@@ -175,18 +190,68 @@ object Prueba {
 
       //sustituir la primera por la segunda en la lista
       if(menu==1){
+        print("La lista original es: ")
+        print(listaPalabras1)
+        println()
         listaPalabras1=sustitucionEnLaLista(pedirPalabra(),pedirPalabra(),listaPalabras1)
+        println()
+        print("La lista modificada es: ")
+        print(listaPalabras1)
+        println()
+
       }
 
       //Eliminar una palabra de la lista
       if(menu==2){
+        print("La lista original es: ")
+        print(listaPalabras1)
+        println()
         listaPalabras1=eliminarPalabra(pedirPalabra(),listaPalabras1)
+        println()
+        print("La lista modificada es: ")
+        print(listaPalabras1)
+        println()
       }
 
       //eliminar de la primera lista los nombres de la segunda lista
       if(menu==3){
+        print("La lista original es: ")
+        print(listaPalabras1)
+        println()
         listaPalabras1=eliminarPalabrasEnLaPrimeraDeLaSegunda(listaPalabras1,listaPalabras2)
+        println()
+        print("La lista modificada es: ")
+        print(listaPalabras1)
+        println()
       }
+
+      //Listas sin repeticiones
+      if(menu==4){
+        print("Las listas originales son: ")
+        println(listaPalabras1)
+        println(listaPalabras2)
+        println("................................")
+        println("Las listas resultantes son: ")
+        println("Lista de palabras que aparecen en las dos listas: ")
+        println(unionDeListas(listaPalabras1,listaPalabras2))
+        println("Lista de palabras que aparecen en la primera lista, pero no en la segunda: ")
+        println(eliminarPalabrasEnLaPrimeraDeLaSegunda(listaPalabras1,listaPalabras2))
+        println("Lista de palabras que aparecen en la segunda lista, pero no en la primera")
+        println(eliminarPalabrasEnLaPrimeraDeLaSegunda(listaPalabras2,listaPalabras1))
+        println("Lista de palabras que aparecen en ambas listas sin repeticiones")
+        println(unionDeListasSinRep(listaPalabras1,listaPalabras2))
+      }
+
+
+
+      //Lista de suma acumulada
+      if(menu==6){
+        println("La lista de numeros original es: ")
+        println(listaNumeros)
+        println("La suma acumulada: ")
+        println(listaSumaAculumada(listaNumeros))
+      }
+
     }
   }
 
